718a4782098e8e401cd19f9edcd1bec9521211ca
Paul Steffen <paul.steffen@edu-werkstatt.de>
2019-07-09 09:27:55 +0200

    add backup- and restore-api implementation

---
+++ backup/moodle2/backup_socialcomments_stepslib.php
@@ -0,0 +1,142 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block socialcomments backup steplib.
+ *
+ * @package    block_socialcomments
+ * @subpackage backup-moodle2
+ * @copyright  2019 Paul Steffen, EDU-Werkstatt GmbH
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Block socialcomments backup structure step class.
+ *
+ * @copyright  2019 Paul Steffen, EDU-Werkstatt GmbH
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class backup_socialcomments_block_structure_step extends backup_block_structure_step {
+
+    public function execute() {
+        parent::execute();
+    }
+
+    protected function define_structure() {
+        // To know if we are including userinfo
+        $userinfo = true;// $this->get_setting_value('userinfo');
+        $courseid = $this->get_courseid();
+        $contextid = context_course::instance($courseid)->id;
+
+        $comments = new backup_nested_element('socialcomments');
+        $replies = new backup_nested_element('replies');
+        $pins = new backup_nested_element('pins');
+        $subscriptions = new backup_nested_element('subscriptions');
+
+        // Define each element separated
+        $comment = new backup_nested_element('comment', array('id'), array(
+            'contextid',
+            'component',
+            'commentarea',
+            'itemid',
+            'content',
+            'format',
+            'userid',
+            'groupid',
+            'courseid',
+            'timecreated',
+            'timemodified'
+        ));
+
+        $reply = new backup_nested_element('reply', array('id'), array(
+            'commentid',
+            'content',
+            'format',
+            'userid',
+            'timecreated',
+            'timemodified'
+        ));
+
+        $pin = new backup_nested_element('pin', array('id'), array(
+            'itemtype',
+            'itemid',
+            'userid',
+            'timecreated',
+        ));
+
+        $subscription = new backup_nested_element('subscription', array('id'), array(
+            'courseid',
+            'contextid',
+            'userid',
+            'timelastsent',
+            'timecreated',
+            'timemodified'
+        ));
+
+        // Build the tree
+        $comments->add_child($comment);
+        $comment->add_child($replies);
+        $comments->add_child($pins);
+        $comments->add_child($subscriptions);
+
+        $replies->add_child($reply);
+        $pins->add_child($pin);
+        $subscriptions->add_child($subscription);
+
+        // Define sources
+
+        // All the elements only happen if we are including user info
+        if ($userinfo) {
+            $comment->set_source_sql('SELECT *
+                                       FROM {block_socialcomments_cmmnts}
+                                       WHERE courseid = ?', array(backup::VAR_COURSEID));
+            $reply->set_source_table('block_socialcomments_replies', array('commentid' => '../../id'));
+            $subscription->set_source_sql('SELECT *
+                                       FROM {block_socialcomments_subscrs}
+                                       WHERE courseid = ?', array(backup::VAR_COURSEID));
+
+            /**
+             * We don't use backup::VAR_CONTEXTID in this query since we
+             * require the course contextid, not the block contextid.
+             */
+            $pin->set_source_sql('SELECT p.*
+                                        FROM {block_socialcomments_pins} p
+                                        JOIN {block_socialcomments_cmmnts} c
+                                        ON p.itemid = c.id
+                                        AND p.itemtype = '.block_socialcomments\local\comments_helper::PINNED_COMMENT.'
+                                        AND c.courseid = ?
+                                        UNION
+                                        SELECT p.* FROM {block_socialcomments_pins} p
+                                        WHERE p.itemid = '.$contextid.'
+                                        AND p.itemtype = '.block_socialcomments\local\comments_helper::PINNED_PAGE.'
+                                        ORDER BY id',
+                                    array(
+                                        backup::VAR_COURSEID,
+                                    ));
+        }
+
+        // Define id annotations
+        $comment->annotate_ids('user', 'userid');
+        $comment->annotate_ids('group', 'groupid');
+
+        // Define file annotations
+
+        // Return the root element (socialcomments), wrapped into standard activity structure
+        return $this->prepare_block_structure($comments);
+    }
+}
---
+++ backup/moodle2/restore_socialcomments_stepslib.php
@@ -0,0 +1,125 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block socialcomments restore steplib.
+ *
+ * @package    block_socialcomments
+ * @subpackage backup-moodle2
+ * @copyright  2019 Paul Steffen, EDU-Werkstatt GmbH
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Define the complete socialcomments  structure for restore
+ *
+ * @copyright  2019 Paul Steffen, EDU-Werkstatt GmbH
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class restore_socialcomments_block_structure_step extends restore_structure_step {
+
+    /**
+     * Define structure.
+     */
+    protected function define_structure() {
+        $paths = array();
+
+        $paths[] = new restore_path_element('comment', '/block/socialcomments/comment', true);
+        $paths[] = new restore_path_element('reply', '/block/socialcomments/comment/replies/reply');
+        $paths[] = new restore_path_element('pin', '/block/socialcomments/pins/pin', true);
+        $paths[] = new restore_path_element('subscription', '/block/socialcomments/subscriptions/subscription', true);
+        return $paths;
+    }
+
+    /**
+     * Restore comment entries.
+     *
+     * @global type $DB
+     * @param type $data
+     */
+    public function process_comment($data) {
+        global $DB;
+        $data = (object)$data;
+        $oldid = $data->id;
+        $data->courseid = $this->get_courseid();
+        $data->contextid = context_course::instance($data->courseid)->id;
+        $data->userid = $this->get_mappingid('user', $data->userid);
+        //ToDo: check if groupid mapping required
+
+        $newitemid = $DB->insert_record('block_socialcomments_cmmnts', $data);
+        $this->set_mapping('block_socialcomments_cmmnts', $oldid, $newitemid);
+
+        if (isset($data->replies['reply'])) {
+            foreach ($data->replies['reply'] as $reply) {
+                $this->process_comment_reply($reply);
+            }
+        }
+    }
+
+    /**
+     * Restore reply entries.
+     *
+     * @global type $DB
+     * @param type $data
+     */
+    public function process_comment_reply($data) {
+        global $DB;
+        $data = (object)$data;
+        $oldid = $data->id;
+        $data->commentid = $this->get_mappingid('block_socialcomments_cmmnts', $data->commentid);
+        $data->userid = $this->get_mappingid('user', $data->userid);
+        $newitemid = $DB->insert_record('block_socialcomments_replies', $data);
+        $this->set_mapping('block_socialcomments_replies', $oldid, $newitemid);
+    }
+
+    /**
+     * Restore pin entries.
+     *
+     * @global type $DB
+     * @param type $data
+     */
+    public function process_pin($data) {
+      global $DB;
+      $data = (object)$data;
+      $courseid = $this->get_courseid();
+      $data->userid = $this->get_mappingid('user', $data->userid);
+
+      if ($data->itemtype == block_socialcomments\local\comments_helper::PINNED_PAGE) {
+          $data->itemid = context_course::instance($courseid)->id;
+      } elseif ($data->itemtype == block_socialcomments\local\comments_helper::PINNED_COMMENT) {
+          $data->itemid =  $this->get_mappingid('block_socialcomments_cmmnts', $data->itemid);
+      }
+      $newitemid = $DB->insert_record('block_socialcomments_pins', $data);
+    }
+
+    /**
+     * Restore subscription entries.
+     *
+     * @global type $DB
+     * @param type $data
+     */
+    public function process_subscription($data) {
+        global $DB;
+        $data = (object)$data;
+        $data->courseid = $this->get_courseid();
+        $data->contextid = context_course::instance($data->courseid)->id;
+        $data->userid = $this->get_mappingid('user', $data->userid);
+        //ToDo: check if timelastsent needs to be updated or modified in any way
+        $newitemid = $DB->insert_record('block_socialcomments_subscrs', $data);
+    }
+}
