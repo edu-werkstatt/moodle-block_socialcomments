cfbe2a89345d0f59595107ba66637ba64047255c
Paul Steffen <paul.steffen@edu-werkstatt.de>
2019-07-15 18:00:04 +0200

    check in unit tests

--- tests/privacy_test.php
+++ tests/privacy_test.php
@@ -25,9 +25,13 @@
 defined('MOODLE_INTERNAL') || die();
 
 use \core_privacy\local\metadata\collection;
+use \core_privacy\local\request\approved_contextlist;
 use \core_privacy\local\request\approved_userlist;
 use \core_privacy\tests\provider_testcase;
 use \block_socialcomments\privacy\provider;
+
+global $CFG;
+require_once($CFG->libdir . '/externallib.php');
 
 /**
  * Unit tests for blocks\socialcomments\classes\privacy\provider.php
@@ -51,13 +55,83 @@ class block_socialcomments_testcase extends provider_testcase {
     public function test_get_metadata() {
         $collection = new collection('block_socialcomments');
         $newcollection = provider::get_metadata($collection);
+        $itemcollection = $newcollection->get_collection();
+        $this->assertCount(4, $itemcollection);
+
+        $table = reset($itemcollection);
+        $this->assertEquals('block_socialcomments_cmmnts', $table->get_name());
+        $privacyfields = $table->get_privacy_fields();
+        $this->assertArrayHasKey('contextid', $privacyfields);
+        $this->assertArrayHasKey('content', $privacyfields);
+        $this->assertArrayHasKey('userid', $privacyfields);
+        $this->assertArrayHasKey('groupid', $privacyfields);
+        $this->assertArrayHasKey('courseid', $privacyfields);
+        $this->assertArrayHasKey('timemodified', $privacyfields);
+        $this->assertEquals('privacy:metadata:block_socialcomments_cmmnts', $table->get_summary());
+
+        $table = next($itemcollection);
+        $this->assertEquals('block_socialcomments_subscrs', $table->get_name());
+        $privacyfields = $table->get_privacy_fields();
+        $this->assertArrayHasKey('courseid', $privacyfields);
+        $this->assertArrayHasKey('contextid', $privacyfields);
+        $this->assertArrayHasKey('userid', $privacyfields);
+        $this->assertArrayHasKey('timelastsent', $privacyfields);
+        $this->assertArrayHasKey('timemodified', $privacyfields);
+        $this->assertEquals('privacy:metadata:block_socialcomments_subscrs', $table->get_summary());
+
+        $table = next($itemcollection);
+        $this->assertEquals('block_socialcomments_pins', $table->get_name());
+        $privacyfields = $table->get_privacy_fields();
+        $this->assertArrayHasKey('itemtype', $privacyfields);
+        $this->assertArrayHasKey('itemid', $privacyfields);
+        $this->assertArrayHasKey('userid', $privacyfields);
+        $this->assertEquals('privacy:metadata:block_socialcomments_pins', $table->get_summary());
+
+        $table = next($itemcollection);
+        $this->assertEquals('block_socialcomments_replies', $table->get_name());
+        $privacyfields = $table->get_privacy_fields();
+        $this->assertArrayHasKey('commentid', $privacyfields);
+        $this->assertArrayHasKey('content', $privacyfields);
+        $this->assertArrayHasKey('userid', $privacyfields);
+        $this->assertArrayHasKey('timemodified', $privacyfields);
+        $this->assertEquals('privacy:metadata:block_socialcomments_replies', $table->get_summary());
     }
 
     /**
      * Test getting the context for the user ID related to this plugin.
      */
     public function test_get_contexts_for_userid() {
-    
+        global $DB, $USER;
+
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        $generator = $this->getDataGenerator();
+        $course = $generator->create_course();
+        $coursecontext = context_course::instance($course->id);
+
+        $record = array('courseid' => $course->id, 'name' => 'Group');
+        $group = $generator->create_group($record);
+
+        $user = $generator->create_user();
+        $generator->enrol_user($user->id, $course->id, 'student');
+
+        $generator->create_group_member(array('userid' => $user->id, 'groupid' => $group->id));
+
+        $this->assertEmpty(provider::get_contexts_for_userid($user->id));
+
+        // Create a comment.
+        $this->add_comment($coursecontext, $user, 'Comment0');
+
+        $comment = $DB->get_record('block_socialcomments_cmmnts', array('userid' => $user->id));
+        $this->assertNotFalse($comment);
+
+        $data = $DB->count_records('block_socialcomments_cmmnts');
+        $this->assertEquals(1, $data);
+
+        $contextlist = provider::get_contexts_for_userid($user->id);
+        $this->assertCount(1, $contextlist);
+        $this->assertEquals($coursecontext, $contextlist->current());
     }
 
 
@@ -65,13 +139,80 @@ class block_socialcomments_testcase extends provider_testcase {
      * Test that data is exported correctly for this plugin.
      */
     public function test_export_user_data() {
+        global $DB;
 
+        $this->resetAfterTest();
+        $generator = $this->getDataGenerator();
+        $component = 'block_socialcomments';
+
+        $student = $generator->create_user();
+
+        // Enrol user in course and add course items.
+        $course = $generator->create_course();
+        $coursecontext = context_course::instance($course->id);
+        $generator->enrol_user($student->id, $course->id, 'student');
+
+
+        // Generate some data.
+        $commentid0 = $this->add_comment($coursecontext, $student, 'Comment0');
+        $commentid1 = $this->add_comment($coursecontext, $student, 'Comment1');
+
+        // Confirm data is present.
+        $params = [
+          'contextid' => $coursecontext->id,
+          'userid' => $student->id,
+        ];
+
+        $result = $DB->count_records('block_socialcomments_cmmnts', $params);
+        $this->assertEquals(2, $result);
+
+        // Export data for student.
+        $approvedlist = new approved_contextlist($student, $component, [$coursecontext->id]);
+        provider::export_user_data($approvedlist);
+
+        // Confirm student's data is exported.
+        $writer = \core_privacy\local\request\writer::with_context($coursecontext);
+        $this->assertTrue($writer->has_any_data());
+        var_dump($writer);
     }
 
     /**
      * Test that only users within a course context are fetched.
      */
     public function test_get_users_in_context() {
+        global $DB, $USER;
+
+        $this->resetAfterTest();
+        $generator = $this->getDataGenerator();
+
+        $student = $generator->create_user();
+        $teacher = $generator->create_user();
+
+        // Enrol users in course.
+        $course = $generator->create_course();
+        $generator->enrol_user($student->id, $course->id, 'student');
+        $generator->enrol_user($teacher->id, $course->id, 'editingteacher');
+
+        // Check nothing is found before socialcomments data iscreated.
+        $coursecontext = context_course::instance($course->id);
+        $userlist = new \core_privacy\local\request\userlist($coursecontext, 'block_socialcomments');
+        provider::get_users_in_context($userlist);
+        $this->assertCount(0, $userlist);
+
+        // Generate some data for both users.
+
+        // Crate a pin to course context for teacher.
+        $this->add_pin($coursecontext, $teacher);
+
+        // Create a comment for student.
+        $this->add_comment($coursecontext, $student, 'Comment0');
+
+        $userlist = new \core_privacy\local\request\userlist($coursecontext, 'block_socialcomments');
+        provider::get_users_in_context($userlist);
+        $this->assertCount(2, $userlist);
+        $userids = $userlist->get_userids();
+        $this->assertContains( $student->id, $userids );
+        $this->assertContains( $teacher->id, $userids );
     }
 
 
@@ -93,4 +234,36 @@ class block_socialcomments_testcase extends provider_testcase {
     public function test_delete_data_for_user() {
     }
 
+    /**
+     * Call external API to create a comment.
+     */
+    protected function add_comment($coursecontext, $user, $content = 'Comment') {
+        global $USER;
+        $this->setUser($user);
+        // Needed for calling the webservice without sesskey.
+        $USER->ignoresesskey = true;
+
+        $params = array('contextid' => $coursecontext->id, 'content' => $content, 'groupid' => 0, 'id' => 0);
+        $result = external_api::call_external_function('block_socialcomments_save_comment', $params);
+        $this->assertFalse($result['error']);
+        return $result['data']['id'];
+    }
+
+    /**
+     * Call external API to create a pin.
+     */
+    protected function add_pin($coursecontext, $user, $commentid = 0) {
+        global $USER;
+        $this->setUser($user);
+        // Needed for calling the webservice without sesskey.
+        $USER->ignoresesskey = true;
+        $params = array(
+            'contextid' => $coursecontext->id,
+            'checked' => true,
+            'commentid' => $commentid
+        );
+        $result = external_api::call_external_function('block_socialcomments_set_pinned', $params);
+        $this->assertFalse($result['error']);
+        $this->assertEquals($commentid, $result['data']['commentid']);
+     }
 }
