1f7fa8bf8cdfe71d17ea83ad256b8dc1f606b69d
Paul Steffen <paul.steffen@edu-werkstatt.de>
2019-07-26 10:58:03 +0200

    bump version number

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019042501;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2019072601;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016112900;        // Requires this Moodle version.
 $plugin->component = 'block_socialcomments'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
